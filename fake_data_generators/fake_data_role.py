1ddf323134fad261d763fee8bdfb81512a570259
LizardBob <krzysztof.chrusciel@tivix.com>
2019-11-01 23:50:16 +0100

    added role generator

--- fake_data_generators/fake_data_role.py
+++ fake_data_generators/fake_data_role.py
@@ -1,5 +1,18 @@
 from .set_up_django_for_generators import set_up_django
 from faker import Factory
+from ..league.models import Role, Person
 
 set_up_django()
 fake = Factory.create()
+
+# TODO change fields in Role Model for BooleanField
+def create_roles():
+    list_of_roles = []
+    IS_ADMIN = 'True'
+    IS_MANAGER = 'True'
+    IS_PLAYER = 'True'
+
+    for person in Person.objects.all():
+        list_of_roles.append(Role(is_player=IS_PLAYER, person=person))
+
+    Role.objects.bulk_create(list_of_roles)
