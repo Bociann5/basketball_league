e1fe87d2d069386c95dfe94e1c73fed601f48650
Bociann5 <k.boc-orzechowski@wp.pl>
2019-11-02 01:23:32 +0100

    Add fake_data for players, and players_statistic. Change database for my local DB in settings

---
+++ fake_data_player_statistic.py
@@ -0,0 +1,22 @@
+import os, django
+import random, time
+
+from faker import Faker
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "basketball_league.settings")
+django.setup()
+ 
+from league.models import Player, PlayerStatistic
+
+
+def create_players_statistic():
+    players = Player.objects.all()
+    for player in players:
+        points = random.randrange(0,50)
+        rebounds = random.randrange(0,25)
+        assists = random.randrange(0,25)
+        player_statistic = PlayerStatistic.objects.create(player = player, points=points, 
+                                            rebounds=rebounds, assists=assists)
+        player_statistic.save()
+
+create_players_statistic()
