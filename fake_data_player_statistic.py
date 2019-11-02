03a4b0f1f1279b7bb17c34fe8e6cb272de4bb05b
Bociann5 <k.boc-orzechowski@wp.pl>
2019-11-02 02:31:42 +0100

    clean code

--- fake_data_player_statistic.py
+++ fake_data_player_statistic.py
@@ -10,7 +10,7 @@ from league.models import Player, PlayerStatistic
 
 
 def create_players_statistic():
-    players = Player.objects.all()
+    players = Player.objects.all()[:10]
     for player in players:
         points = random.randrange(0,50)
         rebounds = random.randrange(0,25)
@@ -18,5 +18,6 @@ def create_players_statistic():
         player_statistic = PlayerStatistic.objects.create(player = player, points=points, 
                                             rebounds=rebounds, assists=assists)
         player_statistic.save()
+        print(count)
 
 create_players_statistic()
