a6f1380aee3a783494121014971a816e1a507625
LizardBob <krzysztof.chrusciel@tivix.com>
2019-11-09 00:06:36 +0100

    added generator for league and schedule

---
+++ fake_data_generators/fake_data_schedule.py
@@ -0,0 +1,20 @@
+import set_up_django_for_generators
+
+set_up_django_for_generators.set_up_django()
+
+import time
+from datetime import datetime
+from league.models import Schedule, Game
+
+
+def create_schedule():
+    year = datetime.now().year
+    schedule = Schedule.objects.create(year=year)
+    schedule.games.set(Game.objects.all())
+    schedule.save()
+
+
+start = time.time()
+print('Start', start)
+create_schedule()
+print(f'End! It takes {time.time() - start} seconds')
